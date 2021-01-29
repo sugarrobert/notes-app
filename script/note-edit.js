6b520b087b2d6f69840e70c4d209c1cab6622c28
sugarrobert <robert.sugar@hotmail.com>
2021-01-29 17:04:56 +0100

    new event listener for adding new note in LS

--- script/note-edit.js
+++ script/note-edit.js
@@ -7,5 +7,22 @@ document.querySelector('#delete-note').addEventListener('click', () => {
 });
 
 document.querySelector('#save-changes').addEventListener('click', () => {
-  console.log('save');
+  const noteTitle = document.querySelector('#note-title');
+  const noteBody = document.querySelector('#note-body');
+
+  const note = new Note();
+
+  let title = noteTitle.value;
+  let body = noteBody.value;
+  let created = Date.now();
+  let changed = Date.now();
+  let id = note.generateID();
+
+  const newNote = new Note(title, body, created, changed, id);
+
+  Store.addNote(newNote);
+
+  location.assign('./index.html');
+
+  //console.log(window.location.search);
 });
