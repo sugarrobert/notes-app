46f4bf4afbf426d1305981b9617003ada47a8969
sugarrobert <robert.sugar@hotmail.com>
2021-01-30 20:37:18 +0100

    changed way of saving edited note

--- script/note-edit.js
+++ script/note-edit.js
@@ -1,12 +1,15 @@
-//
+//on page load check for id in url
 document.addEventListener('DOMContentLoaded', () => {
   const queryString = window.location.search;
   if (queryString !== '') {
+    //get id from url
     const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get('id');
 
+    //load notes from LS
     const notes = Store.getNotes();
 
+    //load title and body from LS that match id from url
     notes.forEach((note) => {
       if (note.id === id) {
         const noteTitle = document.querySelector('#note-title');
@@ -19,44 +22,67 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+//return on index page on cancel button
 document.querySelector('#cancel-changes').addEventListener('click', () => {
   location.assign('./index.html');
 });
 
+//delete note on delete button
 document.querySelector('#delete-note').addEventListener('click', () => {
-  console.log('delete');
-});
-
-document.querySelector('#save-changes').addEventListener('click', () => {
+  //check for id in url
   const queryString = window.location.search;
   if (queryString !== '') {
+    //get id from url
     const urlParams = new URLSearchParams(queryString);
     const urlId = urlParams.get('id');
 
+    //delete note from LS
+    Store.removeNote(urlId);
+  }
+
+  //return on index page
+  location.assign('./index.html');
+});
+
+//event listener for save button
+document.querySelector('#save-changes').addEventListener('click', () => {
+  const queryString = window.location.search;
+  //check for id in url, if id is in url replace old value with new
+  if (queryString !== '') {
+    //get id from url
+    const urlParams = new URLSearchParams(queryString);
+    const urlId = urlParams.get('id');
+
+    //load notes from LS
     const notes = Store.getNotes();
 
-    notes.forEach((item) => {
-      if (item.id === urlId) {
-        const noteTitle = document.querySelector('#note-title');
-        const noteBody = document.querySelector('#note-body');
-
-        let title = noteTitle.value;
-        let body = noteBody.value;
-        let created = item.created;
-        let changed = Date.now();
-        let id = urlId;
-
-        const newNote = new Note(title, body, created, changed, id);
-        Store.removeNote(urlId);
-        Store.addNote(newNote);
-      }
-    });
-
-    location.assign('./index.html');
-  } else {
     const noteTitle = document.querySelector('#note-title');
     const noteBody = document.querySelector('#note-body');
 
+    //loop throught notes array and replace value of edited note
+    const mapedNotes = notes.map((item) => {
+      if (item.id === urlId) {
+        item = {
+          ...item,
+          title: noteTitle.value,
+          body: noteBody.value,
+          changed: Date.now(),
+        };
+      }
+      return item;
+    });
+
+    //save new array with new value in LS
+    localStorage.setItem('notes', JSON.stringify(mapedNotes));
+
+    //return on index page
+    location.assign('./index.html');
+  } else {
+    //if there is no id in url save new note
+    const noteTitle = document.querySelector('#note-title');
+    const noteBody = document.querySelector('#note-body');
+
+    //Instantiate note
     const note = new Note();
 
     let title = noteTitle.value;
@@ -65,10 +91,13 @@ document.querySelector('#save-changes').addEventListener('click', () => {
     let changed = Date.now();
     let id = note.generateID();
 
+    //Instantiate new note
     const newNote = new Note(title, body, created, changed, id);
 
+    //add note to LS
     Store.addNote(newNote);
 
+    //return on index page
     location.assign('./index.html');
   }
 });
